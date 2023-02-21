7acde6a055bac8966f59ce3df660d9fcc16c2c7e
od317 <osamadrbas31@gmail.com>
2023-02-21 16:10:55 +0300

    new

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/epic_games_clone/',
   plugins: [vue()],
   devServer: {
     proxy: 'https://store.epicgames.com/en-US',
